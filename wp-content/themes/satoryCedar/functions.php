17c23cebe7b8771c924e0fb683cbfabe10bfd37b
luizfiorentino <lgfiorentino@gmail.com>
2024-12-16 21:50:07 +0100

    registered the navigation menu to functions.php file

---
+++ wp-content/themes/satoryCedar/functions.php
@@ -0,0 +1,11 @@
+<?php
+// Theme setup
+function satory_tree_theme_setup() {
+    register_nav_menus( array(
+        'primary' => __( 'Primary Menu', 'satoryCedar' ),
+    ) );
+}
+add_action( 'after_setup_theme', 'satory_tree_setup' );
+
+// Additional functions
+
