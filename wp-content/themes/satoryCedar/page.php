290187a13099e5604790caeab9d9f59b7607c0e8
luizfiorentino <lgfiorentino@gmail.com>
2025-01-02 18:03:35 +0100

    added code to the page.php file

---
+++ wp-content/themes/satoryCedar/page.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * The main page type
+ * 
+ * @package satoryCedar
+ * @since Satory Cedar 1.0
+ */
+?>
+<?php get_header(); ?> 
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+    
+    <?php if ( have_posts() ) : ?>
+
+        <?php while ( have_posts() ) : the_post(); ?>
+
+            <!-- <h2 class="text-center"><?php the_title(); ?></h2> -->
+            <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink())), '</a></h2>' ) ; ?>
+            <?php the_content(); ?>
+
+            <?php endwhile; ?>
+
+        <?php endif; ?>
+    </div> 
+    </div> 
+</div>
+
+<?php get_footer(); ?>
