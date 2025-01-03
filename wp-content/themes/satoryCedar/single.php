06d062ac42904c70fd6c3bc2907424eee7de53f2
luizfiorentino <lgfiorentino@gmail.com>
2025-01-03 20:06:05 +0100

    added pagination to posts

--- wp-content/themes/satoryCedar/single.php
+++ wp-content/themes/satoryCedar/single.php
@@ -15,10 +15,29 @@
             <?php if ( have_posts() ) : ?>
 
              <?php while ( have_posts() ) : the_post(); ?>
+             <div <?php post_class(); ?>>
 
             <!-- <h2 class="text-center"><?php the_title(); ?></h2> -->
              <h1><?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink())), '</a></h2>' ) ; ?></h1>
+             <?php the_date() ?>
              <?php the_content(); ?>
+
+             <?php 
+             wp_link_pages( array (
+                'before' => '<div class="page-links"><span class="page-links-title">' . __('Pages', 'customtheme') . '</span>', 
+                'after' => '</div>',
+                'link_before' => '<span>',
+                'link_after' => '</span>',
+                'pagelink' => '<span class="screen-reader-text">' . __('Page', 'customtheme') . '</span>%',
+                'separator' => '<span class="screen-reader-text"> , </span>'
+             ))
+             ?>
+             </div>
+             <?php 
+             if( comments_open() || get_comments_number() {
+                comments_template();
+             })
+             ?>
             <?php endwhile; ?>
 
         <?php endif; ?>
