9401ba433b20142dbdb5bc981685bda289a85ff0
luizfiorentino <lgfiorentino@gmail.com>
2025-01-03 16:41:11 +0100

    added navigation links to the posts (next/prev)

--- wp-content/themes/satoryCedar/single.php
+++ wp-content/themes/satoryCedar/single.php
@@ -1,18 +1,43 @@
-<?php get_header(); ?>
+<?php 
+/**
+ * The template for displaying the posts
+ * 
+ * @package satoryCedar
+ * @since Satory Cedar 1.0
+ */
+?>
 
-<main>
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <h1><?php the_title(); ?></h1>
-        <div><?php the_content(); ?></div>
+<?php get_header(); ?> 
 
-        <!-- Display Custom Meta Field Value -->
-        <?php
-        $custom_value = get_post_meta( get_the_ID(), 'custom_subtitle_field', true );
-        if ( ! empty( $custom_value ) ) {
-            echo '<p>Subtitle: ' . esc_html( $custom_value ) . '</p>';
-        }
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <?php if ( have_posts() ) : ?>
+
+             <?php while ( have_posts() ) : the_post(); ?>
+
+            <!-- <h2 class="text-center"><?php the_title(); ?></h2> -->
+             <h1><?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink())), '</a></h2>' ) ; ?></h1>
+             <?php the_content(); ?>
+            <?php endwhile; ?>
+
+        <?php endif; ?>
+
+        <?php 
+            if( is_singular('post')) {
+                the_post_navigation( array (
+                    'next_text' => 'Next',
+                    'prev_text' => 'Previous'
+                ));
+            }
         ?>
-    <?php endwhile; endif; ?>
-</main>
+    </div> 
+
+    <div class="col-md-4">
+      <?php get_sidebar(); ?>
+    </div>
+
+    </div> 
+</div>
 
 <?php get_footer(); ?>
